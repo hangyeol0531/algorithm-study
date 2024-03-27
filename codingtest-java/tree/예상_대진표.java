a78cc9848403dc0931c7106f2a8aee3df90562ae
hangyeol0531 <jhan0531@naver.com>
2024-03-27 23:30:50 +0900

    codingtest-java tree 예상 대진표

---
+++ codingtest-java/tree/예상_대진표.java
@@ -0,0 +1,17 @@
+public class 예상_대진표 {
+
+    public static int solution(int n, int a, int b) {
+        int answer = 0;
+
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(8, 4, 7));
+    }
+}
