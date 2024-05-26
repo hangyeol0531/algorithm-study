8830a5437d113629606df4d85982e1a19e7758ed
hangyeol0531 <jhan0531@naver.com>
2024-05-26 23:24:13 +0900

    codingtest-java graph 경주로 건설 리팩토링 및 최적화

--- codingtest-java/graph/경주로_건설.java
+++ codingtest-java/graph/경주로_건설.java
@@ -1,42 +1,42 @@
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 
 public class 경주로_건설 {
 
     private static final int[] xArr = {0, 0, 1, -1};
     private static final int[] yArr = {1, -1, 0, 0};
+    private static int[][][] isVisited;
 
     public static class Node {
         int x, y, cost, latest;
-        int[][] isVisited;
 
-        public Node(int x, int y, int cost, int latest, int[][] isVisited) {
+        public Node(int x, int y, int cost, int latest) {
             this.x = x;
             this.y = y;
             this.cost = cost;
             this.latest = latest;
-            this.isVisited = isVisited;
         }
-    }
-
-    private static int[][] getArrayDeppCopy(int[][] arr) {
-        return Arrays.stream(arr)
-            .map(int[]::clone)
-            .toArray(int[][]::new);
     }
 
     // 직선 100, 코너 500
     public static int solution(int[][] board) {
         int xLen = board[0].length;
         int yLen = board.length;
+
+        isVisited = new int[yLen][xLen][4];
+        for (int i = 0; i < yLen; i++) {
+            for (int j = 0; j < xLen; j++) {
+                for (int k = 0; k < 4; k++) {
+                    isVisited[i][j][k] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
         int result = Integer.MAX_VALUE;
 
         Queue<Node> queue = new ArrayDeque<>();
-        int[][] initIsVisited = new int[yLen][xLen];
-        initIsVisited[0][0] = 1;
-        queue.add(new Node(0, 0, 0, -1, initIsVisited));
 
+        queue.add(new Node(0, 0, 0, -1));
         while (!queue.isEmpty()) {
             Node poll = queue.poll();
 
@@ -49,21 +49,19 @@ public class 경주로_건설 {
                     nowCost += 500;
                 }
 
-                int[][] isVisited = poll.isVisited;
                 if (nowX < 0 || nowY < 0 || nowX >= xLen || nowY >= yLen) {
                     continue;
                 }
 
-                if (board[nowY][nowX] == 1 || isVisited[nowY][nowX] == 1) {
+                if (board[nowY][nowX] == 1 || isVisited[nowY][nowX][i] < nowCost) {
                     continue;
                 }
 
                 if (nowX == xLen - 1 && nowY == yLen - 1) {
                     result = Math.min(result, nowCost);
                 } else {
-                    int[][] copyIsVisited = getArrayDeppCopy(isVisited);
-                    copyIsVisited[nowY][nowX] = 1;
-                    queue.add(new Node(nowX, nowY, nowCost, i, copyIsVisited));
+                    isVisited[nowY][nowX][i] = nowCost;
+                    queue.add(new Node(nowX, nowY, nowCost, i));
                 }
             }
         }
@@ -72,54 +70,66 @@ public class 경주로_건설 {
 
     public static void main(String[] args) {
         //tc1
-        System.out.println(
-            solution(
-                new int[][]{
-                    {0, 0, 0},
-                    {0, 0, 0},
-                    {0, 0, 0}
-                }
-            )
-        );
+//        System.out.println(
+//            solution(
+//                new int[][]{
+//                    {0, 0, 0},
+//                    {0, 0, 0},
+//                    {0, 0, 0}
+//                }
+//            )
+//        );
 
         //tc2
-        System.out.println(
-            solution(
-                new int[][]{
-                    {0, 0, 0, 0, 0, 0, 0, 1},
-                    {0, 0, 0, 0, 0, 0, 0, 0},
-                    {0, 0, 0, 0, 0, 1, 0, 0},
-                    {0, 0, 0, 0, 1, 0, 0, 0},
-                    {0, 0, 0, 1, 0, 0, 0, 1},
-                    {0, 0, 1, 0, 0, 0, 1, 0},
-                    {0, 1, 0, 0, 0, 1, 0, 0},
-                    {1, 0, 0, 0, 0, 0, 0, 0}
-                }
-            )
-        );
+//        System.out.println(
+//            solution(
+//                new int[][]{
+//                    {0, 0, 0, 0, 0, 0, 0, 1},
+//                    {0, 0, 0, 0, 0, 0, 0, 0},
+//                    {0, 0, 0, 0, 0, 1, 0, 0},
+//                    {0, 0, 0, 0, 1, 0, 0, 0},
+//                    {0, 0, 0, 1, 0, 0, 0, 1},
+//                    {0, 0, 1, 0, 0, 0, 1, 0},
+//                    {0, 1, 0, 0, 0, 1, 0, 0},
+//                    {1, 0, 0, 0, 0, 0, 0, 0}
+//                }
+//            )
+//        );
 
 //        tc3
-        System.out.println(
-            solution(
-                new int[][]{
-                    {0, 0, 1, 0},
-                    {0, 0, 0, 0},
-                    {0, 1, 0, 1},
-                    {1, 0, 0, 0}
-                }
-            )
-        );
+//        System.out.println(
+//            solution(
+//                new int[][]{
+//                    {0, 0, 1, 0},
+//                    {0, 0, 0, 0},
+//                    {0, 1, 0, 1},
+//                    {1, 0, 0, 0}
+//                }
+//            )
+//        );
 //
 //        tc4
+//        System.out.println(
+//            solution(
+//                new int[][]{
+//                    {0, 0, 0, 0, 0, 0},
+//                    {0, 1, 1, 1, 1, 0},
+//                    {0, 0, 1, 0, 0, 0},
+//                    {1, 0, 0, 1, 0, 1},
+//                    {0, 1, 0, 0, 0, 1},
+//                    {0, 0, 0, 0, 0, 0}
+//                }
+//            )
+//        );
+//        tc5
         System.out.println(
             solution(
                 new int[][]{
-                    {0, 0, 0, 0, 0, 0},
-                    {0, 1, 1, 1, 1, 0},
-                    {0, 0, 1, 0, 0, 0},
-                    {1, 0, 0, 1, 0, 1},
-                    {0, 1, 0, 0, 0, 1},
-                    {0, 0, 0, 0, 0, 0}
+                    {0, 0, 0, 0, 0},
+                    {0, 1, 1, 1, 0},
+                    {0, 0, 1, 0, 0},
+                    {1, 0, 0, 0, 1},
+                    {1, 1, 1, 0, 0}
                 }
             )
         );
